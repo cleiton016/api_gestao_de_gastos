62d762b4e2ebde5c99345b6af1cab62944cde0a6
cleiton016 <cleitonluiz2014@gmail.com>
2022-04-15 01:26:29 -0300

    Chore: Reestruturando model

--- app_gestao/models.py
+++ app_gestao/models.py
@@ -3,54 +3,73 @@ from django.db import models
 from django.contrib.auth.models import User 
 # Create your models here.
 
-class Bancos(models.Model):
-    usuario_id = models.ForeignKey(User, related_name='bancos', on_delete=models.CASCADE)
+class Usuario(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=128)
+    email = models.EmailField()
+    foto = models.ImageField(null=True, blank=True)
+
+    criado_em = models.DateTimeField(auto_now_add=True)
+    ultima_alteracao = models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+    def __str__(self):
+        return self.nome
+
+class Banco(models.Model):
+    usuario_fk = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     nome = models.CharField(max_length=24)
     saldo = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
     credito_limite = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
     credito_disponivel = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
-    fechamento_fatura = models.DateField()
-    vencimento_fatura = models.DateField() 
+    fechamento_fatura = models.IntegerField(max_length=2)
+    vencimento_fatura = models.IntegerField(max_length=2) 
 
     criado_em = models.DateTimeField(auto_now_add=True)
-    
+    ultima_alteracao = models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name = "Banco"
+        verbose_name_plural = "Bancos"
     def __str__(self):
         return self.nome
 
-class Categorias(models.Model):
+class Categoria(models.Model):
     nome = models.CharField(max_length=24)
     slug = models.SlugField(max_length=28)
     cor = models.CharField(max_length=16)
 
     criado_em = models.DateTimeField(auto_now_add=True)
-
+    ultima_alteracao = models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
     def __str__(self):
         return self.nome
 
-class Gastos(models.Model):
+class Gasto(models.Model):
     nome = models.CharField(max_length=24)
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
     data_referente = models.DateField()
-        
-    usuario_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    categoria_id = models.ForeignKey(Categorias, on_delete=models.CASCADE)
+    categoria_fk = models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
     criado_em = models.DateTimeField(auto_now_add=True)
-
+    ultima_alteracao = models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name = "Gasto"
+        verbose_name_plural = "Gastos"
     def __str__(self):
-        return self.categoria+'_'+self.criado_em
+        return "{nome} (R$: {valor})".format(nome=self.nome, valor=self.valor)
 
-class Faturas(models.Model):
-    banco_id = models.ForeignKey(Bancos, on_delete=models.CASCADE)
-
-    limite_usado = models.DecimalField(max_digits=7, decimal_places=2)
-    mes_referente = models.DateField()
-    fechada = models.BooleanField(default=False)
+class Lancamento(models.Model):
+    usuario_fk = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    banco_fk = models.ForeignKey(Banco, on_delete=models.CASCADE)
+    gasto_fk = models.OneToOneField(Gasto, on_delete=models.CASCADE)
 
     criado_em = models.DateTimeField(auto_now_add=True)
-
-class GastoValor(models.Model):
-    fatura_id = models.ForeignKey(Faturas, on_delete=models.CASCADE)
-    gasto_id = models.ForeignKey(Gastos, on_delete=models.CASCADE)
-    valor = models.DecimalField(max_digits=7, decimal_places=2)    
-
-    
+    ultima_alteracao = models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name = "Lançamento"
+        verbose_name_plural = "Lançamentos"
+    def __str__(self):
+        return "Lançamento: {}".format(self.gasto_fk)
