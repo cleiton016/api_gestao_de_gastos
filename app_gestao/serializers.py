f4c3a456758d2f45af2b6b57656cc410690bdc6a
cleiton016 <cleitonluiz2014@gmail.com>
2022-04-16 23:17:33 -0300

    Chore: Modificando serializadores

--- app_gestao/serializers.py
+++ app_gestao/serializers.py
@@ -1,24 +1,36 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User, Group, Permission
 from rest_framework import serializers
 from app_gestao.models import *
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'groups']
 
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['id', 'name']
+
+
+class PermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Permission
+        fields = '__all__'
+#---------------------------------------------------------#
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
         fields = '__all__'
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
-#-------------------------------------------------------------------#
-class CategoriaSerializer(serializers.HyperlinkedModelSerializer):
+class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
         fields = '__all__'
 
 class GastoSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Gasto
         fields = '__all__'
