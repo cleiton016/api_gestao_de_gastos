ccbac5b43d5f3ff7197b539d318d4df8dea1bd25
cleiton016 <cleitonluiz2014@gmail.com>
2022-04-16 23:15:40 -0300

    Chore: Configurando Arquivo .env

--- api_controle/settings.py
+++ api_controle/settings.py
@@ -9,20 +9,25 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import environ, os
 from pathlib import Path
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cta$g^*7(r#ryz#*jvo6a=*=7_@&7f5v8@$cu&0wo@6c^5otv2'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -79,15 +84,15 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': "gestao_de_gastos",
+        'NAME':env('DB_NAME'),
 
-        'USER': 'root',
+        'USER': env('DB_USER'),
 
-        'PASSWORD': 'qazx123.',
+        'PASSWORD': env('DB_PASSWORD'),
 
-        'HOST': 'localhost',
+        'HOST': env('DB_HOST'),
 
-        'PORT': '5432',
+        'PORT': env('DB_PORT')
 
     }
 }
@@ -115,9 +120,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Araguaina'
 
 USE_I18N = True
 
