59f4be4b41b2f94bde0d7bf6b854875b71a5a47f
cleiton016 <cleitonluiz2014@gmail.com>
2022-04-20 16:36:29 -0300

    Feature: Criando novas visualizações

--- app_gestao/views.py
+++ app_gestao/views.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
+from django.contrib.auth.models import User, Group, Permission
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, permissions
 from app_gestao.serializers import *
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,6 +11,17 @@ class UserViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class PermissionSiewSet(viewsets.ModelViewSet):
+    queryset = Permission.objects.all()
+    serializer_class = PermissionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+#---------------------------------------------------------------------#
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 class CategoriaViewSet(viewsets.ModelViewSet):
@@ -27,6 +38,11 @@ class BancoViewSet(viewsets.ModelViewSet):
     queryset = Banco.objects.all()
     serializer_class = BancoSerializer
     permission_classes = [permissions.IsAuthenticated]
+class LancamentoViewSer(viewsets.ModelViewSet):
+    queryset = Lancamento.objects.all()
+    serializer_class = LancamentoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['ano', 'mes','usuario_fk']
 
-        
 
