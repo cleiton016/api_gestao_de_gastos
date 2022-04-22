303da5cf8b3ec70580be817955e6ee5879d9c585
cleiton016 <cleitonluiz2014@gmail.com>
2022-04-22 10:27:57 -0300

    adicionando rotas de autenticação por token

--- api_controle/urls.py
+++ api_controle/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import routers
 from app_gestao import views
 
@@ -12,12 +13,14 @@ router.register(r'usuario', views.UsuarioViewSet)
 router.register(r'categorias', views.CategoriaViewSet)
 router.register(r'gastos', views.GastoViewSet)
 router.register(r'bancos', views.BancoViewSet)
-router.register('lancamento', views.LancamentoViewSer, basename='lancamento')
+router.register(r'lancamento', views.LancamentoViewSer, basename='lancamento')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenObtainPairView.as_view(), name='token_refresh')
 ]
