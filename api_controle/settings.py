fdf1310bdc7b4e387fcd867ef30409358750c03c
cleiton016 <cleitonluiz2014@gmail.com>
2022-04-23 19:50:18 -0300

    atualizando rotas staticas

--- api_controle/settings.py
+++ api_controle/settings.py
@@ -126,11 +126,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-MEDIA_ROOT = '../api_controle/media'
+MEDIA_ROOT = '../api_gestao_de_gastos/media'
 
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = '../api_controle/static'
+STATIC_ROOT = '../api_gestao_de_gastos/static'
 
 STATIC_URL = '/static/'
 
